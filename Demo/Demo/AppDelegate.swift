59214c437c5339d3e30edc6e4e67b5adc433ff92
RomanTysiachnik <pot9ka@gmail.com>
2018-01-19 15:23:45 +0200

    added director

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -4,177 +4,6 @@ import UIKit
 import PlayerControls
 
 typealias Props = ContentControlsViewController.Props
-func pipOnly() -> Props {
-    return Props.player(Props.Player(
-        playlist: Props.Playlist(
-            next: nil,
-            prev: nil),
-        item: .playable(Props.Controls(
-            airplay: .hidden,
-            audible: Props.MediaGroupControl(options: []),
-            camera: Props.Camera(
-                angles: Props.Angles(
-                    horizontal: 0.0,
-                    vertical: 0.0),
-                moveTo: .nop),
-            error: nil,
-            legible: .external(
-                external: .available(state: .active(text: "Somthing short")),
-                control: Props.MediaGroupControl(options: [Props.Option(
-                    name: "Option1",
-                    selected: true,
-                    select: .nop)])),
-            live: Props.Live(
-                isHidden: false,
-                dotColor: nil),
-            loading: false,
-            pictureInPictureControl: .possible(.nop),
-            playbackAction: .play(.nop),
-            seekbar: Props.Seekbar(
-                duration: 3600,
-                currentTime: 1800,
-                progress: 0.5,
-                buffered: 0.7,
-                seeker: Props.Seeker(
-                    seekTo: .nop,
-                    state: Props.State(
-                        start: .nop,
-                        update: .nop,
-                        stop: .nop))),
-            settings: .hidden,
-            sideBarViewHidden: true,
-            thumbnail: nil,
-            title: "Long titel"))))
-}
-
-func pipSettings() -> Props {
-    return Props.player(Props.Player(
-        playlist: Props.Playlist(
-            next: nil,
-            prev: nil),
-        item: .playable(Props.Controls(
-            airplay: .hidden,
-            audible: Props.MediaGroupControl(options: []),
-            camera: Props.Camera(
-                angles: Props.Angles(
-                    horizontal: 0.0,
-                    vertical: 0.0),
-                moveTo: .nop),
-            error: nil,
-            legible: .external(
-                external: .available(state: .active(text: "Somthing short")),
-                control: Props.MediaGroupControl(options: [Props.Option(
-                    name: "Option1",
-                    selected: true,
-                    select: .nop)])),
-            live: Props.Live(
-                isHidden: false,
-                dotColor: nil),
-            loading: false,
-            pictureInPictureControl: .possible(.nop),
-            playbackAction: .play(.nop),
-            seekbar: Props.Seekbar(
-                duration: 3600,
-                currentTime: 1800,
-                progress: 0.5,
-                buffered: 0.7,
-                seeker: Props.Seeker(
-                    seekTo: .nop,
-                    state: Props.State(
-                        start: .nop,
-                        update: .nop,
-                        stop: .nop))),
-            settings: .enabled(.nop),
-            sideBarViewHidden: true,
-            thumbnail: nil,
-            title: "Long titel"))))
-}
-
-func pipAirPlaySettings() -> Props {
-    return Props.player(Props.Player(
-        playlist: Props.Playlist(
-            next: nil,
-            prev: nil),
-        item: .playable(Props.Controls(
-            airplay: .enabled,
-            audible: Props.MediaGroupControl(options: []),
-            camera: Props.Camera(
-                angles: Props.Angles(
-                    horizontal: 0.0,
-                    vertical: 0.0),
-                moveTo: .nop),
-            error: nil,
-            legible: .external(
-                external: .available(state: .active(text: "Somthing short")),
-                control: Props.MediaGroupControl(options: [Props.Option(
-                    name: "Option1",
-                    selected: true,
-                    select: .nop)])),
-            live: Props.Live(
-                isHidden: false,
-                dotColor: nil),
-            loading: false,
-            pictureInPictureControl: .possible(.nop),
-            playbackAction: .play(.nop),
-            seekbar: Props.Seekbar(
-                duration: 3600,
-                currentTime: 1800,
-                progress: 0.5,
-                buffered: 0.7,
-                seeker: Props.Seeker(
-                    seekTo: .nop,
-                    state: Props.State(
-                        start: .nop,
-                        update: .nop,
-                        stop: .nop))),
-            settings: .enabled(.nop),
-            sideBarViewHidden: true,
-            thumbnail: nil,
-            title: "Long titel"))))
-}
-
-func pipAirPlay() -> Props {
-    return Props.player(Props.Player(
-        playlist: Props.Playlist(
-            next: nil,
-            prev: nil),
-        item: .playable(Props.Controls(
-            airplay: .enabled,
-            audible: Props.MediaGroupControl(options: []),
-            camera: Props.Camera(
-                angles: Props.Angles(
-                    horizontal: 0.0,
-                    vertical: 0.0),
-                moveTo: .nop),
-            error: nil,
-            legible: .external(
-                external: .available(state: .active(text: "Somthing short")),
-                control: Props.MediaGroupControl(options: [Props.Option(
-                    name: "Option1",
-                    selected: true,
-                    select: .nop)])),
-            live: Props.Live(
-                isHidden: false,
-                dotColor: nil),
-            loading: false,
-            pictureInPictureControl: .possible(.nop),
-            playbackAction: .play(.nop),
-            seekbar: Props.Seekbar(
-                duration: 3600,
-                currentTime: 1800,
-                progress: 0.5,
-                buffered: 0.7,
-                seeker: Props.Seeker(
-                    seekTo: .nop,
-                    state: Props.State(
-                        start: .nop,
-                        update: .nop,
-                        stop: .nop))),
-            settings: .enabled(.nop),
-            sideBarViewHidden: true,
-            thumbnail: nil,
-            title: "Long titel"))))
-}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -184,27 +13,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let vc = DefaultControlsViewController()
-        vc.view.backgroundColor = .green
+        vc.view.backgroundColor = .red
         vc.view.tintColor = .blue
-        vc.props = pipAirPlaySettings()
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 6, execute: {
-            vc.props = pipSettings()
-        })
+        let propsDirector = PropsDirector()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 12, execute: {
-            vc.props = pipOnly()
-        })
+        propsDirector.buttonProps.append(contentsOf: propsAllButtonsVisible())
+        propsDirector.buttonProps.append(contentsOf: propsTwoButtonsVisible())
+        propsDirector.buttonProps.append(contentsOf: propsOneButtonVisible())
+        vc.props = baseProps()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 18, execute: {
-            vc.props = pipSettings()        })
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 24, execute: {
-            vc.props = pipAirPlaySettings()
-        })
-
-
-
+        if #available(iOS 10.0, *) {
+            let timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { (_) in
+                vc.props = propsDirector.updateProps()
+            }
+        }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
@@ -213,5 +36,167 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+func propsAllButtonsVisible() -> [ButtonsProps] {
+    let props: [ButtonsProps] = [
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .possible(.nop))
+    ]
+    return props
+}
 
+func propsTwoButtonsVisible() -> [ButtonsProps] {
+    let props: [ButtonsProps] = [
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported)
+    ]
+    return props
+}
 
+func propsOneButtonVisible() -> [ButtonsProps] {
+    let props: [ButtonsProps] = [
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .possible(.nop)),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .possible(.nop)),
+        
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .hidden, pipState: .possible(.nop)),
+        
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported),
+        ButtonsProps(settingsState: .hidden, airplayState: .enabled, pipState: .unsupported),
+        ButtonsProps(settingsState: .enabled(.nop), airplayState: .hidden, pipState: .unsupported)
+        ]
+    return props
+}
+
+func baseProps() -> Props {
+    return Props.player(Props.Player(
+        playlist: Props.Playlist(
+            next: .nop,
+            prev: .nop),
+        item: .playable(Props.Controls(
+            airplay: .enabled,
+            audible: Props.MediaGroupControl(options: []),
+            camera: Props.Camera(
+                angles: Props.Angles(
+                    horizontal: 0.0,
+                    vertical: 0.0),
+                moveTo: .nop),
+            error: nil,
+            legible: .external(
+                external: .available(state: .active(text: "Somthing short")),
+                control: Props.MediaGroupControl(options: [Props.Option(
+                    name: "Option1",
+                    selected: true,
+                    select: .nop)])),
+            live: Props.Live(
+                isHidden: false,
+                dotColor: nil),
+            loading: false,
+            pictureInPictureControl: .possible(.nop),
+            playbackAction: .play(.nop),
+            seekbar: Props.Seekbar(
+                duration: 3600,
+                currentTime: 1800,
+                progress: 0.5,
+                buffered: 0.7,
+                seeker: Props.Seeker(
+                    seekTo: .nop,
+                    state: Props.State(
+                        start: .nop,
+                        update: .nop,
+                        stop: .nop))),
+            settings: .enabled(.nop),
+            sideBarViewHidden: false,
+            thumbnail: nil,
+            title: "Long titel"))))
+}
+
+func sideProps() -> [SideBarView.ButtonProps]{
+    
+    let shareIcons = SideBarView.ButtonProps.Icons(
+        normal: UIImage(named: "icon-share", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+        selected: nil,
+        highlighted: UIImage(named: "icon-share-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+    
+    let share = SideBarView.ButtonProps(
+        isEnabled: true,
+        isSelected: false,
+        icons: shareIcons,
+        handler: .nop)
+    
+    let addIcon = SideBarView.ButtonProps.Icons(
+        normal: UIImage(named: "icon-add", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+        selected: nil,
+        highlighted: UIImage(named: "icon-add-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+    
+    let add = SideBarView.ButtonProps(
+        isEnabled: true,
+        isSelected: false,
+        icons: addIcon,
+        handler: .nop)
+    
+    let favoriteIcon = SideBarView.ButtonProps.Icons(
+        normal: UIImage(named: "icon-fav", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+        selected: nil,
+        highlighted: UIImage(named: "icon-fav-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+    
+    let favorite = SideBarView.ButtonProps(
+        isEnabled: true,
+        isSelected: false,
+        icons: favoriteIcon,
+        handler: .nop)
+    
+    let laterIcon = SideBarView.ButtonProps.Icons(
+        normal: UIImage(named: "icon-later", in: Bundle(for: SideBarView.self), compatibleWith: nil)!,
+        selected: nil,
+        highlighted: UIImage(named: "icon-later-active", in: Bundle(for: SideBarView.self), compatibleWith: nil))
+    
+    let later = SideBarView.ButtonProps(
+        isEnabled: true,
+        isSelected: false,
+        icons: laterIcon,
+        handler: .nop)
+    
+    return [later, favorite, share, add]
+}
